6b41b514871aaf7ef050c53787c4129f8ad63b94
mohan murali <mohanmuralid28@gmail.com>
2024-01-07 09:19:18 +0000

    admin route change

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,1 +1,22 @@
 const { Router } = require("express");
+const ElectionModel = require("../models/election");
+
+const adminRouter = Router();
+
+adminRouter.put("/election", authHandler, async (req, res) => {
+  try {
+    const { status } = req;
+    const election = await ElectionModel.findOneAndUpdate({}, { status });
+    res.status(200).json({
+      success: true,
+      election,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: `could not update the status of election`,
+    });
+  }
+});
+
+module.exports = adminRouter;
