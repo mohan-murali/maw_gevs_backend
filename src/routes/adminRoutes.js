422cf32a8acca07f25c780bbbb97dc90e391cb0d
mohan murali <mohanmuralid28@gmail.com>
2024-01-08 13:42:56 +0000

    Admin routes

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const ElectionModel = require("../models/election");
+const authHandler = require("../middleware/authHandler");
 
 const adminRouter = Router();
 
@@ -15,6 +16,21 @@ adminRouter.put("/election", authHandler, async (req, res) => {
     res.status(500).json({
       success: false,
       message: `could not update the status of election`,
+    });
+  }
+});
+
+adminRouter.get("/election-status", authHandler, async (req, res) => {
+  try {
+    const election = await ElectionModel.findOne({});
+    res.status(200).json({
+      success: true,
+      election,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "could not retrieve election result",
     });
   }
 });
