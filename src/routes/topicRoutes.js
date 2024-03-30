54069903e0c2d694d53aa02c5753b016296cc7e5
mohan murali <mohanmuralid28@gmail.com>
2024-03-30 17:04:25 +0000

    updated get endpoint

--- src/routes/topicRoutes.js
+++ src/routes/topicRoutes.js
@@ -4,29 +4,28 @@ const authHandler = require("../middleware/authHandler");
 
 const topicRouter = Router();
 
-topicRouter.post("/add", authHandler, async (req, res) => {
+topicRouter.post("/add-topic", authHandler, async (req, res) => {
   try {
     const { topic, supervisor, details, isCustom } = req.body;
     if (!topic || !supervisor || !details) {
       res.status(400).json({
         success: false,
-        message: "topic, supervisor, details"
-      })
+        message: "topic, supervisor, details",
+      });
     }
 
-    const newTopic = new TopicModel( {
+    const newTopic = new TopicModel({
       topic,
       supervisor,
       details,
       isCustom,
-      isApproved:false
-    })
+      isApproved: false,
+    });
     await newTopic.save();
 
     res.status(200).json({
       success: true,
     });
-
   } catch (e) {
     res.status(500).json({
       success: false,
@@ -37,12 +36,12 @@ topicRouter.post("/add", authHandler, async (req, res) => {
 
 topicRouter.get("/update-topic", async (req, res) => {
   try {
-    const { id, topic, supervisor, details} = req.body
+    const { id, topic, supervisor, details } = req.body;
     await TopicModel.findByIdAndUpdate(id, {
       topic,
       details,
-      supervisor
-    })
+      supervisor,
+    });
     res.status(200).json({
       success: true,
     });
@@ -56,15 +55,31 @@ topicRouter.get("/update-topic", async (req, res) => {
 
 topicRouter.get("/delete-topic", async (req, res) => {
   try {
-    const { id } = req.body
-    const response = await TopicModel.findByIdAndDelete(id)
+    const { id } = req.body;
+    const response = await TopicModel.findByIdAndDelete(id);
     res.status(200).json({
       success: true,
     });
   } catch (e) {
     res.status(500).json({
       success: false,
-      message: "could not update the topic",
+      message: "could not delete the topic",
+    });
+  }
+});
+
+topicRouter.get("/get-topic", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const topics = await TopicModel.find({});
+    res.status(200).json({
+      success: true,
+      topics,
+    });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      message: "could not fetch the topic",
     });
   }
 });
